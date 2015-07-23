e4404a9851e6636b3d221a203c5ae2759bfb74d5
Lucas Fryzek <nercos7@gmail.com>
2015-07-22 21:55:24 -0400

    add read bsp header

--- bsp.c
+++ bsp.c
@@ -1,12 +1,57 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "bsp.h"
+#include "types.h"
+
+#define BSP_VERSION 29
+
+typedef struct {
+	int32_t offset;
+	int32_t size;
+} BspEntry;
+
+typedef struct {
+	int32_t version;
+	BspEntry entities;
+	BspEntry planes;
+
+	BspEntry miptex;
+	BspEntry vertices;
+
+	BspEntry visilist;
+	BspEntry nodes;
+
+	BspEntry texinfo;
+
+	BspEntry faces;
+
+	BspEntry lightmaps;
+	BspEntry clipnodes;
+
+	BspEntry leaves;
+
+	BspEntry lface;
+	BspEntry edges;
+
+	BspEntry ledges;
+	BspEntry models;
+} BspHeader;
 
 struct s_Bsp {
 };
 
 Bsp *readBsp(void *BspData){
-	Bsp *bsp = malloc(sizeof(Bsp));
+	BspHeader header;
+	
+	memcpy(&header, BspData, sizeof(BspHeader));
+	if(header.version != BSP_VERSION){
+		printf("Invalid BSP version expected %d found %d\n",
+				BSP_VERSION, header.version);
+		return NULL;
+	}
 
+	Bsp *bsp = malloc(sizeof(Bsp));
 	return bsp;
 }
 
--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include "pak.h"
+#include "bsp.h"
 #include "palette.h"
 
 int main(){
 	PakFile *pf = readPakFile("id1/pak0.pak");
 	Palette *plt = readPalette(readFile(pf, "gfx/palette.lmp", NULL));
+	
+	Bsp *bsp = readBsp(readFile(pf, "maps/start.bsp", NULL));
 
-	for(int i=0; i<256; i++){
-		colourUByte3 c = getPaletteColour(plt, i);
-		printf("Colour is %d %d %d\n", c.r, c.g, c.b);
-	}
-
+	freeBsp(bsp);
 	freePalette(plt);
 	freePak(pf);
 	return 0;
